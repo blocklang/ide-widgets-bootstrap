585473c6b701dd403f88717488c847ccb86952b2
xiaohulu <idocument@qq.com>
2019-10-10 16:23:23 +0800

    为 TextInput 配置属性面板

---
+++ src/text-input/propertiesLayout.ts
@@ -0,0 +1,14 @@
+import Value from "../property/Value";
+
+export default [
+	{
+		propertyName: "value",
+		propertyLabel: "值",
+		propertyWidget: Value
+	} /*,
+    {
+        propertyName: "onValue",
+        propertyLabel: "输入值事件",
+        propertyWidget: ""
+    }*/
+];
