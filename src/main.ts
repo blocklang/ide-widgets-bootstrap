5188f54dcbb3c215bb25010580e2acb1cc4ee2e3
xiaohulu <idocument@qq.com>
2019-10-10 16:24:05 +0800

    往 PageDesigner 中注册 TextInput 部件

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import global from "@dojo/framework/shim/global";
+import * as blocklang from "designer-core/blocklang";
 import TextInput from "./text-input";
+import TextInputPropertiesLayout from "./text-input/propertiesLayout";
 
-// TODO：为防止用户恶意修改，本文件的代码将在编译期间自动生成
-// 1. 统一在 global._block_lang_widgets_ 中存放部件，是一个全局变量
-if (!global._block_lang_widgets_) {
-	global._block_lang_widgets_ = {};
-}
-// key 的值与部件的文件夹名相同
-var widgetMap = { "text-input": TextInput };
-// 使用 {website}/{owner}/{repoName} 唯一标识组件库中的 Widget
-// 是否需要版本号？因为只加载了一个版本，所以这里不需要版本号做区分
-global._block_lang_widgets_["github.com/blocklang/ide-widgets-bootstrap"] = widgetMap;
+/*******************************/
+/*****往设计器中注册 Widget******/
+/*******************************/
+
+// 一个问题：
+// 如果在这里写了组件仓库所在的网址，如 github.com
+// 那么在 gitee 中备份该仓库，然后从 gitee 开始安装，则这里的值却依然是 github.com
+// 如果动态获取呢？
+const gitUrlSegment = { website: "github.com", owner: "blocklang", repoName: "ide-widgets-bootstrap" };
+const widgets = {
+	"text-input": { widget: TextInput, propertiesLayout: TextInputPropertiesLayout }
+};
+blocklang.registerWidgets(gitUrlSegment, widgets);
