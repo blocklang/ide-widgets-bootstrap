cb6f81a5cf23a53aed9ecfca76126856e5a44114
xiaohulu <idocument@qq.com>
2019-10-10 16:22:55 +0800

    重新设计器版 TextInput

--- src/text-input/index.tsx
+++ src/text-input/index.tsx
@@ -1,10 +1,4 @@
-import { create, tsx } from "@dojo/framework/core/vdom";
+import TextInputBase from "widgets-bootstrap/text-input";
+import { WidgetDesignableMixin } from "designer-core/mixins/WidgetDesignable";
 
-export interface TextInputProperties {}
-
-const factory = create().properties<TextInputProperties>();
-
-export default factory(function TextInput({ properties }) {
-	const {} = properties();
-	return <div>Text Input</div>;
-});
+export default class TextInput extends WidgetDesignableMixin(TextInputBase) {}
