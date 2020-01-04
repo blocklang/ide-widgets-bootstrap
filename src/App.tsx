31be40567458271818f8af1e79e39ab77faeb3e0
xiaohulu <idocument@qq.com>
2020-01-04 16:40:48 +0800

    支持 dev 和 prod 两种构建

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import { create, tsx, invalidator } from "@dojo/framework/core/vdom";
+import theme from "@dojo/framework/core/middleware/theme";
+import dojo from "@dojo/themes/dojo";
+import TextInput from "./text-input";
+import PlainText from "./plain-text";
+import { AttachedWidget, ChangedPropertyValue } from "designer-core/interfaces";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import * as css from "./App.m.css";
+
+const factory = create({ theme, invalidator });
+
+const plainTextWidget: AttachedWidget = {
+	id: "plain-text-1",
+	parentId: "1",
+	apiRepoId: 1,
+	widgetId: 1,
+	widgetName: "PlainText",
+	widgetCode: "0001",
+	canHasChildren: false,
+	properties: [
+		{
+			id: "1",
+			isExpr: false,
+			code: "0001",
+			name: "value",
+			valueType: "string",
+			value: "Hello World!"
+		}
+	]
+};
+
+const textInputWidget: AttachedWidget = {
+	id: "text-input-1",
+	parentId: "1",
+	apiRepoId: 1,
+	widgetId: 2,
+	widgetName: "TextInput",
+	widgetCode: "0002",
+	canHasChildren: false,
+	properties: [
+		{
+			id: "2",
+			isExpr: false,
+			code: "0002",
+			name: "value",
+			valueType: "string",
+			value: "Text Input"
+		}
+	]
+};
+
+export default factory(function App({ middleware: { theme, invalidator } }) {
+	if (!theme.get()) {
+		theme.set(dojo);
+	}
+
+	return (
+		<div classes={[css.root]}>
+			<fieldset>
+				<legend>PlainText</legend>
+				<PlainText
+					widget={plainTextWidget}
+					extendProperties={{
+						onFocused: () => {},
+						onFocusing: () => {},
+						onHighlight: () => {},
+						onUnhighlight: () => {},
+						onPropertyChanged: (changedProperty: ChangedPropertyValue) => {
+							plainTextWidget.properties[0].value = changedProperty.newValue;
+							invalidator();
+						}
+					}}
+					value={plainTextWidget.properties[0].value}
+				/>
+			</fieldset>
+
+			<fieldset>
+				<legend>TextInput</legend>
+				<TextInput
+					widget={textInputWidget}
+					extendProperties={{
+						onFocused: () => {},
+						onFocusing: () => {},
+						onHighlight: () => {},
+						onUnhighlight: () => {}
+					}}
+					value="Text Input"
+				/>
+			</fieldset>
+		</div>
+	);
+});
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,4 @@
-import * as blocklang from "designer-core/blocklang";
-import TextInput from "./text-input";
-import TextInputBase from "widgets-bootstrap/text-input";
-import TextInputPropertiesLayout from "./text-input/propertiesLayout";
-import { widgetInstanceMap } from "@dojo/framework/core/vdom";
-import { ExtensionWidgetMap, GitUrlSegment } from "designer-core/interfaces";
-
-/*******************************/
-/*****往设计器中注册 Widget******/
-/*******************************/
-
-// 一个问题：
-// 如果在这里写了组件仓库所在的网址，如 github.com
-// 那么在 gitee 中备份该仓库，然后从 gitee 开始安装，则这里的值却依然是 github.com
-// 如果动态获取呢？
-const gitUrlSegment: GitUrlSegment = { website: "github.com", owner: "blocklang", repoName: "ide-widgets-bootstrap" };
-const widgets: ExtensionWidgetMap = {
-	TextInput: { widget: TextInputBase, ideWidget: TextInput, propertiesLayout: TextInputPropertiesLayout }
-};
-blocklang.registerWidgets(gitUrlSegment, widgets);
-
-blocklang.cacheWidgetInstanceMap(gitUrlSegment, widgetInstanceMap);
+`has('production')`;
+import "./prod";
+`!has('production')`;
+import "./dev";
