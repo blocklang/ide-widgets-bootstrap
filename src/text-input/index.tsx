ab2eaa95b88d12009b35658b0287f5602f111f36
xiaohulu <idocument@qq.com>
2020-01-14 11:36:19 +0800

    将 TextInput 部件转换为基于函数的部件 (#4)

--- src/text-input/index.tsx
+++ src/text-input/index.tsx
@@ -1,8 +1,30 @@
-import TextInputBase from "widgets-bootstrap/text-input";
-import { WidgetDesignableMixin } from "designer-core/mixins/WidgetDesignable";
+import { create, tsx } from "@dojo/framework/core/vdom";
+import { TextInputProperties } from "widgets-bootstrap/text-input";
+import ide from "designer-core/middleware/ide";
+import Overlay from "designer-core/widgets/overlay";
+import * as css from "./index.m.css";
+import * as c from "bootstrap-classes";
 
-export default class TextInput extends WidgetDesignableMixin(TextInputBase) {
-	protected needOverlay(): boolean {
-		return true;
-	}
-}
+const factory = create({ ide }).properties<TextInputProperties>();
+
+export default factory(function TextInput({ properties, middleware: { ide } }) {
+	ide.config("root");
+	ide.tryFocus();
+
+	const { value = "", onValue } = properties();
+	return (
+		<virtual>
+			<input
+				key="root"
+				classes={[css.root, c.form_control]}
+				value={value}
+				oninput={(event: Event) => {
+					event.stopPropagation();
+					const value = (event.target as HTMLInputElement).value;
+					onValue && onValue(value);
+				}}
+			/>
+			<Overlay {...ide.getFocusNodeOffset()} {...ide.activeWidgetEvents()} />
+		</virtual>
+	);
+});
---
+++ tests/unit/text-input/index.tsx
@@ -0,0 +1,44 @@
+const { describe, it } = intern.getInterface("bdd");
+
+import assertionTemplate from "@dojo/framework/testing/assertionTemplate";
+import harness from "@dojo/framework/testing/harness";
+import { tsx } from "@dojo/framework/core/vdom";
+import TextInput from "../../../src/text-input";
+import ide from "designer-core/middleware/ide";
+import createMockIde from "designer-core/testing/mocks/middleware/ide";
+import Overlay from "designer-core/widgets/overlay";
+import * as css from "../../../src/text-input/index.m.css";
+import * as c from "bootstrap-classes";
+
+describe("TextInput", () => {
+	const baseAssertion = assertionTemplate(() => (
+		<virtual>
+			<input key="root" classes={[css.root, c.form_control]} value="" oninput={() => {}} />
+			<Overlay
+				top={1}
+				left={2}
+				height={3}
+				width={4}
+				onmouseout={() => {}}
+				onmouseover={() => {}}
+				onmouseup={() => {}}
+			/>
+		</virtual>
+	));
+
+	it("default properties", () => {
+		const mockIde = createMockIde();
+		mockIde("root", {
+			position: { top: 1, left: 2, right: 10, bottom: 11 },
+			offset: { top: 1, left: 2, height: 3, width: 4 },
+			size: { height: 3, width: 4 },
+			scroll: { top: 1, left: 2, height: 3, width: 4 },
+			client: { top: 1, left: 2, height: 3, width: 4 }
+		});
+		const h = harness(() => <TextInput widget={{ id: "1" }} extendProperties={{}} />, {
+			middleware: [[ide, mockIde]]
+		});
+
+		h.expect(baseAssertion);
+	});
+});
