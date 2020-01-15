4f8f742e2b6909035ddf9cf249b93e8116e82f72
xiaohulu <idocument@qq.com>
2020-01-15 09:35:02 +0800

    添加示例

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { create, tsx, invalidator } from "@dojo/framework/core/vdom";
+import dimensions from "@dojo/framework/core/middleware/dimensions";
 import theme from "@dojo/framework/core/middleware/theme";
 import dojo from "@dojo/themes/dojo";
+import { add } from "@dojo/framework/stores/state/operations";
 import TextInput from "./text-input";
 import PlainText from "./plain-text";
-import { AttachedWidget, ChangedPropertyValue } from "designer-core/interfaces";
-
+import { AttachedWidget, ChangedPropertyValue, State } from "designer-core/interfaces";
+import store from "designer-core/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import * as css from "./App.m.css";
+import PageData from "./page-data/edit";
+import Store from "@dojo/framework/stores/Store";
 
-const factory = create({ theme, invalidator });
+const factory = create({ theme, store, dimensions, invalidator });
 
 const plainTextWidget1: AttachedWidget = {
 	id: "plain-text-1",
@@ -50,13 +54,33 @@ const plainTextWidget2: AttachedWidget = {
 	]
 };
 
-const textInputWidget: AttachedWidget = {
+const pageDataWidget: AttachedWidget = {
 	id: "text-input-1",
 	parentId: "1",
 	apiRepoId: 1,
 	widgetId: 2,
-	widgetName: "TextInput",
+	widgetName: "PageData",
 	widgetCode: "0002",
+	canHasChildren: false,
+	properties: [
+		{
+			id: "2",
+			isExpr: false,
+			code: "0002",
+			name: "dataId",
+			valueType: "string",
+			value: "2"
+		}
+	]
+};
+
+const textInputWidget: AttachedWidget = {
+	id: "text-input-1",
+	parentId: "1",
+	apiRepoId: 1,
+	widgetId: 3,
+	widgetName: "TextInput",
+	widgetCode: "0003",
 	canHasChildren: false,
 	properties: [
 		{
@@ -70,10 +94,39 @@ const textInputWidget: AttachedWidget = {
 	]
 };
 
-export default factory(function App({ middleware: { theme, invalidator } }) {
+export default factory(function App({ middleware: { theme, store, dimensions, invalidator } }) {
 	if (!theme.get()) {
 		theme.set(dojo);
 	}
+
+	store.executor(((storeObject: Store<State>) => {
+		storeObject.apply([
+			add(storeObject.path("pageModel", "data"), [
+				{
+					id: "1",
+					parentId: "-1",
+					name: "$",
+					type: "Object",
+					open: true
+				},
+				{
+					id: "2",
+					parentId: "1",
+					name: "a",
+					type: "Object",
+					open: false
+				},
+				{
+					id: "3",
+					parentId: "2",
+					name: "label",
+					type: "String",
+					value: "value",
+					open: false
+				}
+			])
+		]);
+	}) as any);
 
 	return (
 		<div classes={[css.root]}>
@@ -82,11 +135,20 @@ export default factory(function App({ middleware: { theme, invalidator } }) {
 				<PlainText
 					widget={plainTextWidget1}
 					extendProperties={{
-						onFocused: () => {},
-						onFocusing: () => {},
-						onHighlight: () => {},
-						onUnhighlight: () => {},
+						onFocused: () => {
+							console.log("plain-text: onfocused");
+						},
+						onFocusing: () => {
+							console.log("plain-text: onFocusing");
+						},
+						onHighlight: () => {
+							console.log("plain-text: onHighlight");
+						},
+						onUnhighlight: () => {
+							console.log("plain-text: onUnhighlight");
+						},
 						onPropertyChanged: (changedProperty: ChangedPropertyValue) => {
+							console.log("plain-text: onPropertyChanged");
 							plainTextWidget1.properties[0].value = changedProperty.newValue;
 							invalidator();
 						}
@@ -107,6 +169,29 @@ export default factory(function App({ middleware: { theme, invalidator } }) {
 						}
 					}}
 					value={plainTextWidget2.properties[0].value}
+				/>
+			</fieldset>
+
+			<fieldset>
+				<legend>PageData</legend>
+				<PageData
+					widget={pageDataWidget}
+					extendProperties={{
+						onFocused: () => {
+							console.log("onFocused");
+						},
+						onFocusing: () => {
+							console.log("onFocusing");
+						},
+						onHighlight: () => {
+							console.log("onHighlight");
+						},
+						onUnhighlight: () => {
+							console.log("onUnhighlight");
+						}
+					}}
+					middlewares={{ dimensions }}
+					dataId="2"
 				/>
 			</fieldset>
 
